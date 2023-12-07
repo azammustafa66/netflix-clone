ad5f7b7e94e05d5cd599657066e0ae3d6a3d9e77
Azam Mustufa Didagur <azammustafa66@gmail.com>
2023-12-07 13:30:03 +0530

    Add Loader component to Browse page

--- src/components/pages/feed/Browse.tsx
+++ src/components/pages/feed/Browse.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import useFetchMovies from "../../../utils/hooks/useFetchMovies";
 import Hero from "./Hero";
 import Movies from "./Movies";
+import { useRecoilValue } from "recoil";
+import { trailersAtom } from "../../../utils/recoil-atoms/trailersAtom";
+import Loader from "../../loader-ui/Loader";
 
 const Browse: React.FC = () => {
+  const { link } = useRecoilValue(trailersAtom);
   useFetchMovies();
 
-  return (
+  return link === "" ? (
+    <Loader />
+  ) : (
     <div className="relative z-0 bg-black w-full min-h-screen overflow-hidden">
       <Hero />
       <Movies />
